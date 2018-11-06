f0099671f3bcc70fc62b05420ec269cf57c4b5af
Falgun <falgunatportal911@gmail.com>
2018-11-06 12:13:49 +0530

    Commit for remaining support files

---
+++ p.py
@@ -0,0 +1,10 @@
+import re
+import sys
+
+from pip._internal import main as _main
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(_main())
+
+
